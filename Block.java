4287856fb3bffb9c8b5bd2a97e1b7c7fc9238a9d
komangjuliana <114323613+komangjuliana@users.noreply.github.com>
2022-12-29 23:25:47 +0800

    greenfoot 2

--- Block.java
+++ Block.java
@@ -14,6 +14,6 @@ public class Block extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        turn(2);
     }
 }
--- RobotWorld.java
+++ RobotWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
+ * @author (Widi) 
  * @version (a version number or a date)
  */
 public class RobotWorld extends World
@@ -16,9 +16,10 @@ public class RobotWorld extends World
     public RobotWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1); 
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -27,5 +28,7 @@ public class RobotWorld extends World
     {
         Robot robot = new Robot();
         addObject(robot,58,60);
+        Block block = new Block();
+        addObject(block,364,206);
     }
 }
