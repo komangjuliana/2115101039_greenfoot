24eb2f657d57ae02d43ad71671f97b7240419a20
komangjuliana <114323613+komangjuliana@users.noreply.github.com>
2022-12-29 23:26:44 +0800

    greenfoot 3

--- RobotWorld.java
+++ RobotWorld.java
@@ -27,8 +27,39 @@ public class RobotWorld extends World
     private void prepare()
     {
         Robot robot = new Robot();
-        addObject(robot,58,60);
+        addObject(robot,48,50);
+        
         Block block = new Block();
-        addObject(block,364,206);
+        addObject(block,427,145);
+        
+        Wall wall = new Wall();
+        addObject(wall, 52, 147);
+        Wall wall2 = new Wall();
+        addObject(wall2, 159, 147);
+        Wall wall3 = new Wall();
+        addObject(wall3, 266, 147);
+        Wall wall4 = new Wall();
+        addObject(wall4, 587, 147);
+        Wall wall5 = new Wall();
+        addObject(wall5, 692, 147);
+        Wall wall6 = new Wall();
+        addObject(wall6, 791, 147);
+        
+        Home home = new Home();
+        addObject(home, 751, 552);
+        
+        ScorePanel score = new ScorePanel();
+        addObject(score, 71, 554);
+        
+        Pizza pizza = new Pizza();
+        addObject(pizza, 720, 46);
+        Pizza pizza2 = new Pizza();
+        addObject(pizza2, 433, 38);
+        Pizza pizza3 = new Pizza();
+        addObject(pizza3, 183, 302);
+        Pizza pizza4 = new Pizza();
+        addObject(pizza4, 682, 312);
+        Pizza pizza5 = new Pizza();
+        addObject(pizza5, 417, 537);
     }
 }
